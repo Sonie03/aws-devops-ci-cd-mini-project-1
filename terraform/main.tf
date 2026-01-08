92c55fd246a6cd437a47742dab8e1db729241643
Sonie03 <sonieemmanuvel@gmail.com>
2026-01-08 16:47:03 +0530

    Configure AWS provider and EC2 instance
    Add AWS provider and EC2 instance configuration

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,1 +1,12 @@
-The file is created
+provider "aws"{
+ region =  "ap-south-1"
+}
+resource "aws_instance" "web"{
+  ami = "ami-0f5ee92e2d63afc18"
+instance_type = "t2.micro"
+key_name ="Devops-key"
+
+tags={
+Name = "Devops-web-server"
+}
+}
