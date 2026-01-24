9f0840289fef0d0f1c0723ff087089bf6d691365
Sonie03 <sonieemmanuvel@gmail.com>
2026-01-24 20:50:37 +0530

    script written for the status execution of nginx

--- scripts/health_check.sh
+++ scripts/health_check.sh
@@ -1,1 +1,10 @@
-The file is created
+#!/bin/bash
+
+STATUS=$( systemctl is-active nginx)
+
+if [ "$STATUS" != "active" ]; then
+echo "Nginx service is NOT running"
+exit 1
+else
+echo "Nginx is running"
+fi
